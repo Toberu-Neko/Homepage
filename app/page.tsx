ff58bc3c0ac990f05373853f68252638c7586f88
ToberuNeko <uni.toberuneko@gmail.com>
2025-07-17 00:12:00 +0900

    tailwind test

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
-      <Link href="/users">Users</Link>
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-indigo-200 to-white">
+      <h1 className="text-red-500 text-4xl">Hello Tailwind</h1>
+      <h1 className="text-5xl font-bold text-indigo-700">Hello, I’m Toberu Neko!</h1>
+      <p className="text-lg mt-4 text-gray-700">Welcome to my portfolio site. 🐾</p>
     </main>
   )
 }
