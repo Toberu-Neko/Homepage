74102fbfa202b973a6a4f7dc66caaa2e2970ea0f
ToberuNeko <uni.toberuneko@gmail.com>
2025-07-19 00:44:16 +0900

    Added sample assets and nav bar.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
+import Navbar from './components/Navbar'
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-indigo-200 to-white">
-      <h1 className="text-red-500 text-4xl">Hello Tailwind</h1>
-      <h1 className="text-5xl font-bold text-indigo-700">Hello, I’m Toberu Neko!</h1>
-      <p className="text-lg mt-4 text-gray-700">Welcome to my portfolio site. 🐾</p>
+    <main>
+      <Navbar></Navbar>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-indigo-200 to-white">
+        <h1 className="text-5xl font-bold text-indigo-700">Hello, I’m Toberu Neko!</h1>
+        <p className="text-lg mt-4 text-gray-700">Welcome to my portfolio site. 🐾</p>
+      </div>
     </main>
   )
 }
